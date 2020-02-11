a109c4d6a56c0376ceee8e488935d372fe2bbac7
omchaudhari122 <60606931+omchaudhari122@users.noreply.github.com>
2020-02-11 12:26:28 +0530

    Add files via upload

---
+++ exam/PyramidPattern.java
@@ -0,0 +1,23 @@
+package exam;
+
+public class PyramidPattern {
+   
+	public static void main(String[] args) {
+       
+		int rows = 5, a = 0;
+        
+		for(int i = 1; i <= rows; ++i, a = 0) {
+        
+			for(int j = 1; j <= rows - i; ++j) {
+                System.out.print("  ");
+            }
+            
+			while(a != 2 * i - 1) {
+                System.out.print("* ");
+                ++a;
+            }
+            
+			System.out.println();
+        }
+    }
+}
---
+++ exam/SumOfArray.java
@@ -0,0 +1,25 @@
+package exam;
+
+import java.util.Scanner;
+public class SumOfArray {
+    
+	public static void main(String args[]) {
+      
+		Scanner obj = new Scanner(System.in);
+      
+		int[] array = new int[5];
+        int sum = 0;
+      
+        System.out.println("Enter the elements:");
+      
+        for (int i = 0; i < 5; i++) {
+            array[i] = obj.nextInt();
+        }
+       
+        for (int number : array) {
+            sum = sum + number;
+        }
+        
+        System.out.println("Sum of array elements is:" + sum);
+    }
+}
